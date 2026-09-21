7959bbf461d37d091c74f908f076cc81cf83c846
abot8639 <abot8639@gmail.com>
2026-09-21 10:02:39 +0300

    refactor: remove legacy compatibility re-export files and update test imports

--- lib/model/ip_article.dart
+++
@@ -1,7 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/model/ip_article.dart
-library;
-
-export 'package:particles_network/src/factory/particle_factory.dart';
-export 'package:particles_network/src/physics/gravity_config.dart';
-export 'package:particles_network/src/physics/particle_controller.dart';
-export 'package:particles_network/src/physics/particle_updater.dart';
