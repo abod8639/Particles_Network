d2453473f202196fca5b9f448a2b1626dc238697
abot8639 <abot8639@gmail.com>
2026-09-21 10:03:09 +0300

    refactor: consolidate library exports into main entrypoint and update test imports

--- lib/model/trajectory_buffer.dart
+++
@@ -1,4 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/model/trajectory_buffer.dart
-library;
-
-export 'package:particles_network/src/core/trajectory_buffer.dart';
