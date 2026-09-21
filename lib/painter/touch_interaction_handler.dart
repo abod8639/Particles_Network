8b9905f0184794935720dcbd86656b6fcdc0a7d5
abot8639 <abot8639@gmail.com>
2026-09-21 10:03:58 +0300

    refactor: remove legacy re-export files and update tests to import main package

--- lib/painter/touch_interaction_handler.dart
+++
@@ -1,5 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/painter/touch_interaction_handler.dart
-library;
-
-export 'package:particles_network/src/interaction/touch_features.dart';
-export 'package:particles_network/src/interaction/touch_interaction_handler.dart';
