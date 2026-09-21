d1af7ee76e1aba619aae58b0f75e6f5baccdd470
abot8639 <abot8639@gmail.com>
2026-09-21 10:02:32 +0300

    refactor: remove deprecated re-export files and update test imports

--- lib/model/default_particle_factory.dart
+++
@@ -1,4 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/model/default_particle_factory.dart
-library;
-
-export 'package:particles_network/src/factory/default_particle_factory.dart';
