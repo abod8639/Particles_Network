46b93f70c08b2fa3db2a297c13d19ac04493a745
abot8639 <abot8639@gmail.com>
2026-09-21 10:03:48 +0300

    refactor: remove unused spatial and painter files and update test suites

--- lib/painter/performance_utils.dart
+++
@@ -1,4 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/painter/performance_utils.dart
-library;
-
-export 'package:particles_network/src/rendering/performance_utils.dart';
