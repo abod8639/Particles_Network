d2e5fae06901b66ba017a7d684f07f3376a6f1e3
abot8639 <abot8639@gmail.com>
2026-09-21 10:03:41 +0300

    refactor: remove obsolete spatial grid, quad tree, and painter utility files and update tests

--- lib/painter/particle_filter.dart
+++
@@ -1,4 +0,0 @@
-/// Re-export for backwards compatibility with package:particles_network/painter/particle_filter.dart
-library;
-
-export 'package:particles_network/src/rendering/particle_filter.dart';
